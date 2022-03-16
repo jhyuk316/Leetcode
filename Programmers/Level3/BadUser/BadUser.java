80d1918be3778c58a493e226b57f7607892874a5
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2022-03-17 01:33:44 +0900

    badUser

--- Programmers/Level3/BadUser/BadUser.java
+++ Programmers/Level3/BadUser/BadUser.java
@@ -3,17 +3,12 @@ package Programmers.Level3.BadUser;
 // https://programmers.co.kr/learn/courses/30/lessons/64064
 
 import java.util.*;
-import java.util.regex.Pattern;
 
 class Solution {
-    boolean[] visited;
     Set<Set<String>> allAnswer = new HashSet<>();
 
     public int solution(String[] user_id, String[] banned_id) {
-        Arrays.sort(banned_id);
-
         allAnswer = new HashSet<>();
-        visited = new boolean[user_id.length];
         dfs(user_id, banned_id, 0, new HashSet<>());
 
         for (Set<String> answer : allAnswer) {
@@ -28,84 +23,18 @@ class Solution {
             allAnswer.add(new HashSet<>(answerSet));
             return;
         }
+
         String pattern = banned_id[j].replace("*", ".");
         for (int i = 0; i < user_id.length; ++i) {
-            if (!visited[i] && Pattern.matches(pattern, user_id[i])
-                    && !answerSet.contains(user_id[i])) {
+            if (!answerSet.contains(user_id[i]) && user_id[i].matches(pattern)) {
                 // System.out.println(user_id[i] + " " + pattern);
-                visited[i] = true;
                 answerSet.add(user_id[i]);
                 dfs(user_id, banned_id, j + 1, answerSet);
                 answerSet.remove(user_id[i]);
-                visited[i] = false;
             }
         }
         return;
     }
-}
-
-
-class Solution1 {
-    public int solution(String[] user_id, String[] banned_id) {
-        int answer = 0;
-        System.out.println(Arrays.toString(banned_id));
-
-        Set<String> res = new HashSet<>();
-        for (int i = 0; i < banned_id.length; ++i) {
-            Set<String> temp = match(user_id, banned_id[i]);
-            System.out.println(banned_id[i] + " " + temp);
-            answer += count(res, temp);
-            res.addAll(temp);
-            System.out.println(answer);
-        }
-
-        return answer;
-    }
-
-    private Set<String> match(String[] user_id, String banned_id) {
-        String pattern = banned_id.replace("*", ".");
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < user_id.length; ++i) {
-
-            if (Pattern.matches(pattern, user_id[i])) {
-                set.add(user_id[i]);
-            }
-        }
-        return set;
-    }
-
-    private int count(Set<String> s1, Set<String> s2) {
-        Set<String> retainSet = new HashSet<>(s1);
-        retainSet.retainAll(s2);
-        System.out.println("retainSet: " + retainSet.size());
-
-        int combi = retainSet.size() * (retainSet.size() - 1) / 2;
-
-        return s1.size() * s2.size() - (retainSet.size() * retainSet.size()) + combi;
-    }
-
-    // }private int count(Set<String> s1, Set<String> s2) {
-    // Set<String> sumSet = new HashSet<>(s1);
-    // sumSet.addAll(s2);
-    //
-    // Set<String> retainSet = new HashSet<>(s1);
-    // retainSet.removeAll(s2);
-    //
-    // Set<String> pureS1 = new HashSet<>(s1);
-    // pureS1.removeAll(retainSet);
-    //
-    // Set<String> pureS2 = new HashSet<>(s2);
-    // pureS2.removeAll(retainSet);
-    //
-    // Set<String> complement = new HashSet<>();
-    // complement.addAll(pureS1);
-    // complement.addAll(pureS2);
-    //
-    // int combi = retainSet.size() * (retainSet.size() - 1) / 2;
-    //
-    // return pureS1.size() * pureS2.size() + retainSet.size() * complement.size() + combi;
-    // }
-
 }
 
 
