2b86feeaa9ed534052a25077ec9e8c29e16f375d
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2022-08-15 13:33:55 +0900

    13

--- String/RomanToInteger/RomanToInteger.java
+++ String/RomanToInteger/RomanToInteger.java
@@ -5,7 +5,43 @@ package String.RomanToInteger;
 import java.util.HashMap;
 import java.util.Map;
 
+// O(N) 단순 무식 매칭, TC가 적은지 이것도 효과적ㅎ
 class Solution {
+    public int romanToInt(String s) {
+        Map<String, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> cMap = new HashMap<>();
+
+        sMap.put("CM", 900);
+        sMap.put("CD", 400);
+        sMap.put("XC", 90);
+        sMap.put("XL", 40);
+        sMap.put("IX", 9);
+        sMap.put("IV", 4);
+
+        cMap.put('M', 1000);
+        cMap.put('D', 500);
+        cMap.put('C', 100);
+        cMap.put('L', 50);
+        cMap.put('X', 10);
+        cMap.put('V', 5);
+        cMap.put('I', 1);
+
+        int res = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (i + 1 < s.length() && sMap.containsKey(s.substring(i, i + 2))) {
+                res += sMap.get(s.substring(i, i + 2));
+                i++;
+            } else {
+                res += cMap.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
+}
+
+
+// O(N) Greedy 앞글자는 무조건 뒷글자보다 크다.
+class Solution1 {
     public int romanToInt(String s) {
         Map<Character, Integer> romanMap = new HashMap<>();
         romanMap.put('I', 1);
