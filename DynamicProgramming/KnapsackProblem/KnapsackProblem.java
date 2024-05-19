b1917a6f7a8f1fbd9147f2662f00b9207c058e93
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2024-05-19 10:12:18 +0900

    백준 12865 냅섹 문제

--- DynamicProgramming/KnapsackProblem/KnapsackProblem.java
+++ DynamicProgramming/KnapsackProblem/KnapsackProblem.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class KnapsackProblem {
+
+class KnapsackProblem {
 
     public static class Item {
         final int weight;
@@ -64,3 +65,67 @@ public class KnapsackProblem {
         System.out.println(dp[N][K]);
     }
 }
+
+class KnapsackProblemLessMemory {
+
+    public static class Item {
+        final int weight;
+        final int value;
+
+        public Item(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+
+    /*
+    4 7
+    6 13
+    4 8
+    3 6
+    5 12
+     */
+    // public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N, K;
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < N; ++i) {
+            int weight = sc.nextInt();
+            int value = sc.nextInt();
+            items.add(new Item(weight, value));
+        }
+
+        int[] curDp = new int[K + 1];
+        int[] preDp = new int[K + 1];
+
+        for (int i = 1; i < N + 1; ++i) {
+            curDp = new int[K + 1];
+            for (int j = 1; j < K + 1; ++j) {
+                Item item = items.get(i - 1);
+
+                curDp[j] = Math.max(preDp[j], curDp[j - 1]);
+                if (j - item.getWeight() >= 0) {
+                    curDp[j] = Math.max(curDp[j], preDp[j - item.getWeight()] + item.getValue());
+                }
+            }
+            preDp = curDp;
+        }
+
+        System.out.println(curDp[K]);
+    }
+}
+
